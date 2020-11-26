7011b382d3e45a5b38a1f7feb27510245f2abb91
johnom-commits <johnom2012@yandex.ru>
2020-11-26 23:02:34 +0300

    Use wrapper

--- src/main/java/ru/job4j/todo/persistence/HbmStore.java
+++ src/main/java/ru/job4j/todo/persistence/HbmStore.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -14,6 +15,7 @@ import ru.job4j.todo.domain.Item;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 public class HbmStore implements TodoDAO, AutoCloseable {
     private static final Logger LOG = LogManager.getLogger(HbmStore.class.getName());
@@ -36,56 +38,48 @@ public class HbmStore implements TodoDAO, AutoCloseable {
         return Lazy.INSTANCE;
     }
 
-    public void create(final Item item) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            session.save(item);
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
             LOG.error(e.getMessage(), e);
+            throw e;
+        } finally {
+            session.close();
         }
     }
 
+    public void create(final Item item) {
+        tx(session -> session.save(item));
+    }
+
     public List<Item> getList(final boolean allTasks) {
-        List result = new ArrayList<>();
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
+        return tx(session -> {
             final var query = new StringBuilder("from Item ");
             if (!allTasks) {
                 query.append("where done = false ");
             }
             query.append("order by id desc ");
-            result = session.createQuery(query.toString()).list();
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            LOG.error(e.getMessage(), e);
-        }
-        return result;
+            return session.createQuery(query.toString()).list();
+        });
     }
 
     public Optional<Item> findById(final int id) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            Item result = session.get(Item.class, id);
-            session.getTransaction().commit();
-            return Optional.of(result);
-        } catch (HibernateException e) {
-            LOG.error(e.getMessage(), e);
-        }
-        return Optional.empty();
+        return tx(session -> Optional.of(session.get(Item.class, id)));
     }
 
     public void update(final int id, final Item item) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
+        tx(session -> {
             Query query = session.createQuery("update Item set done = :newDone where id = :id");
             query.setParameter("newDone", item.isDone());
             query.setParameter("id", id);
-            query.executeUpdate();
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            LOG.error(e.getMessage(), e);
-        }
+            return query.executeUpdate();
+        });
     }
 
     @Override
